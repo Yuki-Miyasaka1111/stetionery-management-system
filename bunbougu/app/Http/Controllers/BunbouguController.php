1358d4c0f357e579b5689e51a3097e8e0f5bd331
Yuki <diyhb1369@gmail.com>
2023-02-21 19:59:05 +0900

    first commit

---
+++ bunbougu/app/Http/Controllers/BunbouguController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bunbougu;
+use App\Models\Bunrui;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BunbouguController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // $bunbougus = Bunbougu::latest()->paginate(5);
+        $bunbougus = Bunbougu::select([
+                'b.id',
+                'b.name',
+                'b.kakaku',
+                'b.shosai',
+                'r.str as bunrui',
+            ])
+            ->from('bunbougus as b')
+            ->join('bunruis as r', function($join) {
+                $join->on('b.bunrui', '=', 'r.id');
+            })
+            ->orderBy('b.id', 'DESC')
+            ->paginate(5);
+
+        return view('index',compact('bunbougus'))
+            ->with('page_id',request()->page)
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $bunruis = Bunrui::all();
+        return view('create')
+            ->with('bunruis', $bunruis);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @return \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:20',
+            'kakaku' => 'required|integer',
+            'bunrui' => 'required|integer',
+            'shosai' => 'required|max:140',
+        ]);
+
+        $bunbougu = new Bunbougu;
+        $bunbougu->name = $request->input(["name"]);
+        $bunbougu->kakaku = $request->input(["kakaku"]);
+        $bunbougu->bunrui = $request->input(["bunrui"]);
+        $bunbougu->shosai = $request->input(["shosai"]);
+        $bunbougu->save();
+
+        return redirect()->route('bunbougus.index')->with('success', '文房具を登録しました');
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @return \App\Models\Bunbougu $bunbougu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bunbougu $bunbougu)
+    {
+        $bunruis = Bunrui::all();
+        return view('show',compact('bunbougu'))
+            ->with('page_id',request()->page_id)
+            ->with('bunruis',$bunruis);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * 
+     * @return \App\Models\Bunbougu $bunbougu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bunbougu $bunbougu)
+    {
+        $bunruis = Bunrui::all();
+        return view('edit', compact('bunbougu'))
+            ->with('bunruis', $bunruis);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @return \Illuminate\Http\Request
+     * @return \App\Models\Bunbougu $bunbougu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bunbougu $bunbougu)
+    {
+        $request->validate([
+            'name' => 'required|max:20',
+            'kakaku' => 'required|integer',
+            'bunrui' => 'required|integer',
+            'shosai' => 'required|max:140',
+        ]);
+            
+        $bunbougu->name = $request->input(["name"]);
+        $bunbougu->kakaku = $request->input(["kakaku"]);
+        $bunbougu->bunrui = $request->input(["bunrui"]);
+        $bunbougu->shosai = $request->input(["shosai"]);
+        // $bunbougu->user_id = Auth::user()->id;
+        $bunbougu->save();
+        
+        return redirect()->route('bunbougus.index')->with('success','文房具を更新しました');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bunbougu $bunbougu)
+    {
+        $bunbougu->delete();
+        return redirect()->route('bunbougus.index')
+            ->with('success','文房具'.$bunbougu->name.'を削除しました');
+    }
+}
---
+++ bunbougu/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+@extends('app')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="text-left">
+                <h2 style="font-size:1rem;">文房具マスター</h2>
+            </div>
+            <div class="text-right">
+            <a class="btn btn-success" href="{{ route('bunbougu.create') }}">新規登録</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success mt-1"><p>{{ $message }}</p></div>
+            @endif
+        </div>
+    </div>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>name</th>
+            <th>kakaku</th>
+            <th>bunrui</th>
+            <th></th>
+            <th></th>
+        </tr>
+        @foreach ($bunbougus as $bunbougu)
+        <tr>
+            <td style="text-align:right">{{ $bunbougu->id }}</td>
+            <td><a href="{{ route('bunbougu.show',$bunbougu->id) }}?page_id={{ $page_id }}">{{ $bunbougu->name }}</a></td>
+            <td style="text-align:left">{{ $bunbougu->kakaku }}円</td>
+            <td style="text-align:left">{{ $bunbougu->bunrui }}</td>
+            <td style="text-align:center">
+                <a class="btn btn-primary" href="{{ route('bunbougu.edit', $bunbougu->id) }}">変更</a>
+            </td>
+            <td style="text-align:center">
+                <form action="{{ route('bunbougu.destroy',$bunbougu->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-sm btn-danger" onclick='return confirm("  削除しますか？");'>削除</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $bunbougus->links('pagination::bootstrap-5') !!}
+@endsection
